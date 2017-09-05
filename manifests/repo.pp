5cfd57919eb0116c82d76a782697e02d7d93604d
Toby McLaughlin <toby@jarpy.net>
2017-09-05 06:02:32 +0200

    PDK skeleton

---
+++ manifests/repo.pp
@@ -0,0 +1,10 @@
+# elastic_stack::repo
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include elastic_stack::repo
+class elastic_stack::repo {
+}
