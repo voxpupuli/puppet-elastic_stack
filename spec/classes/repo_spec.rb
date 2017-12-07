57346a368a0c4251880023153a90f5df0c8799ac
Toby McLaughlin <toby@jarpy.net>
2017-12-07 15:08:51 +1100

    Some visual (only) changes to the tests

--- spec/classes/repo_spec.rb
+++ spec/classes/repo_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
 
-rpm_key_cmd = 'rpmkeys --import https://artifacts.elastic.co/GPG-KEY-elasticsearch'
-
 def url(format, version)
   "https://artifacts.elastic.co/packages/#{version}/#{format}"
 end
@@ -23,30 +21,25 @@ end
 
 describe 'elastic_stack::repo', type: 'class' do
   default_params = {}
+  rpm_key_cmd = 'rpmkeys --import https://artifacts.elastic.co/GPG-KEY-elasticsearch'
+
   on_supported_os(facterversion: '2.4').each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
 
-      describe 'distro-specific package repositories' do
-        case facts[:os]['family']
-        when 'Debian'
-          it { is_expected.to declare_apt }
-        when 'RedHat'
-          it { is_expected.to declare_yum }
-        when 'Suse'
-          it { is_expected.to declare_zypper }
-          it { is_expected.to contain_exec('elastic_suse_import_gpg').with(command: rpm_key_cmd) }
-          it {
-            is_expected.to contain_exec('elastic_zypper_refresh_elastic')
-              .with(command: 'zypper refresh elastic')
-          }
-        end
+      case facts[:os]['family']
+      when 'Debian'
+        it { is_expected.to declare_apt }
+      when 'RedHat'
+        it { is_expected.to declare_yum }
+      when 'Suse'
+        it { is_expected.to declare_zypper }
+        it { is_expected.to contain_exec('elastic_suse_import_gpg').with(command: rpm_key_cmd) }
+        it { is_expected.to contain_exec('elastic_zypper_refresh_elastic').with(command: 'zypper refresh elastic') }
       end
 
-      describe 'overriding version' do
-        let(:params) do
-          default_params.merge(version: 5)
-        end
+      context 'with "version => 5"' do
+        let(:params) { default_params.merge(version: 5) }
 
         case facts[:os]['family']
         when 'Debian'
@@ -58,10 +51,8 @@ describe 'elastic_stack::repo', type: 'class' do
         end
       end
 
-      describe 'overriding priority' do
-        let(:params) do
-          default_params.merge(priority: 99)
-        end
+      context 'with "priority => 99"' do
+        let(:params) { default_params.merge(priority: 99) }
 
         case facts[:os]['family']
         when 'Debian'
@@ -73,21 +64,8 @@ describe 'elastic_stack::repo', type: 'class' do
         end
       end
 
-      describe 'overriding proxy' do
-        let(:params) do
-          default_params.merge(proxy: 'http://proxy.com:8080')
-        end
-
-        case facts[:os]['family']
-        when 'RedHat'
-          it { is_expected.to declare_yum(proxy: 'http://proxy.com:8080') }
-        end
-      end
-
-      describe 'overriding prerelease' do
-        let(:params) do
-          default_params.merge(prerelease: true)
-        end
+      context 'with "prerelease => true"' do
+        let(:params) { default_params.merge(prerelease: true) }
 
         case facts[:os]['family']
         when 'Debian'
@@ -97,6 +75,12 @@ describe 'elastic_stack::repo', type: 'class' do
         when 'Suse'
           it { is_expected.to declare_zypper(version: '6.x-prerelease') }
         end
+      end
+
+      context 'with proxy parameter' do
+        let(:params) { default_params.merge(proxy: 'http://proxy.com:8080') }
+
+        it { is_expected.to declare_yum(proxy: 'http://proxy.com:8080') } if facts[:os]['family'] == 'RedHat'
       end
     end
   end
